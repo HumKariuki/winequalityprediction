2a48a3dff12c793b38a1f3592510c30ff4537946
Hum9679 <kariukihumfrey@gmail.com>
2024-04-16 15:18:50 +0300

    update

--- winewebapp.py
+++ winewebapp.py
@@ -1,15 +1,9 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Apr 15 14:19:41 2024
-
-@author: LENOVO
-"""
 import numpy as np
 import pickle
 import streamlit as st
 
 # Loading the saved model
-loaded_model = pickle.load(open("train2_model.sav", 'rb'))
+loaded_model = pickle.load(open('train2_model.sav', 'rb'))
 
 def wineprediction(input_data):
     # Changing the input data to numpy array
@@ -25,28 +19,35 @@ def wineprediction(input_data):
         return 'This is a poor quality wine😒'
 
 def main():
-    # Giving a title
-    st.title('Wine Prediction Web App')
-    fixed_acidity = st.text_input('Fixed Acidity')
-    volatile_acidity = st.text_input('Volatile Acidity')
-    citric_acid = st.text_input('Citric Acid')
-    residual_sugar = st.text_input('Residual Sugar')
-    chlorides = st.text_input('Chlorides')
-    free_sulfur_dioxide = st.text_input('Free Sulfur Dioxide')
-    total_sulfur_dioxide = st.text_input('Total Sulfur Dioxide')
-    density = st.text_input('Density')
-    ph = st.text_input('pH')
-    sulphates = st.text_input('Sulphates')
-    alcohol = st.text_input('Alcohol')
-   
-    # Code for prediction
-    wine = ''
-   
-    # Creating a button for prediction
-    if st.button('Wine Result'):
-        wine = wineprediction([fixed_acidity, volatile_acidity, citric_acid, residual_sugar, chlorides, free_sulfur_dioxide, total_sulfur_dioxide, density, ph, sulphates, alcohol])
-       
-    st.success(wine)
-   
+    # Sidebar for navigation
+    selected = st.sidebar.radio("Prediction Type", ["Wine Quality Prediction"])
+
+    # Wine Quality Prediction Page
+    if selected == "Wine Quality Prediction":
+        # Giving a title
+        st.title('Wine Quality Prediction Web App')
+
+        # Input fields for wine attributes
+        fixed_acidity = st.text_input('Fixed Acidity')
+        volatile_acidity = st.text_input('Volatile Acidity')
+        citric_acid = st.text_input('Citric Acid')
+        residual_sugar = st.text_input('Residual Sugar')
+        chlorides = st.text_input('Chlorides')
+        free_sulfur_dioxide = st.text_input('Free Sulfur Dioxide')
+        total_sulfur_dioxide = st.text_input('Total Sulfur Dioxide')
+        density = st.text_input('Density')
+        ph = st.text_input('pH')
+        sulphates = st.text_input('Sulphates')
+        alcohol = st.text_input('Alcohol')
+
+        # Code for prediction
+        wine = ''
+
+        # Creating a button for prediction
+        if st.button('Predict Wine Quality'):
+            wine = wineprediction([fixed_acidity, volatile_acidity, citric_acid, residual_sugar, chlorides, free_sulfur_dioxide, total_sulfur_dioxide, density, ph, sulphates, alcohol])
+
+        st.success(wine)
+
 if __name__ == '__main__':
     main()
